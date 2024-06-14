4da3075246cc88a927f305ec5ef3a98781c62191
neurlang <77860779+neurlang@users.noreply.github.com>
2024-06-14 21:28:26 +0200

    walk vals

--- walk.go
+++ walk.go
@@ -1,10 +1,18 @@
 package levenshtein
 
 func Walk[T Number](mat []T, width uint, to func(uint, uint)) {
+	WalkVals(mat, width, func(prev T, this T, x uint, y uint) bool {
+		to(x, y)
+		return false
+	})
+}
+
+func WalkVals[T Number](mat []T, width uint, to func(prev T, this T, x uint, y uint) bool) {
 	pos := uint(len(mat) - 1)
 	x := (pos % width)
 	y := (pos / width)
 	for x > 0 && y > 0 {
+		here := x + width*y
 		diag := x - 1 + width*(y-1)
 		up := x + width*(y-1)
 		left := x - 1 + width*(y)
@@ -12,29 +20,39 @@ func Walk[T Number](mat []T, width uint, to func(uint, uint)) {
 			if mat[diag] < mat[left] && mat[diag] < mat[up] {
 				x--
 				y--
-				to(x, y)
+				if to(mat[here], mat[diag], x, y) {
+					return
+				}
 				continue
 			}
 		}
 		if left >= 0 && up >= 0 {
 			if mat[up] < mat[left] {
 				y--
-				to(x, y)
+				if to(mat[here], mat[up], x, y) {
+					return
+				}
 				continue
 			} else {
 				x--
-				to(x, y)
+				if to(mat[here], mat[left], x, y) {
+					return
+				}
 				continue
 			}
 		}
 		if left >= 0 {
 			x--
-			to(x, y)
+			if to(mat[here], mat[left], x, y) {
+				return
+			}
 			continue
 		}
 		if up >= 0 {
 			y--
-			to(x, y)
+			if to(mat[here], mat[up], x, y) {
+				return
+			}
 			continue
 		}
 	}
